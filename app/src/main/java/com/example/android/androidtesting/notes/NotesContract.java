e3fe1c2d10011e4f4c3abb1f2d663becfccb7534
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-09-24 10:26:24 +0100

    notes interface

--- app/src/main/java/com/example/android/androidtesting/notes/NotesContract.java
+++ app/src/main/java/com/example/android/androidtesting/notes/NotesContract.java
@@ -1,4 +1,34 @@
 package com.example.android.androidtesting.notes;
 
+import android.support.annotation.NonNull;
+
+import com.example.android.androidtesting.data.Note;
+
+import java.util.List;
+
+/**
+ * This specifies the contract between the view and the presenter.
+ */
+
 public interface NotesContract {
+
+    interface View {
+
+        void setProgressIndicator(boolean active);
+
+        void showNotes(List<Note> notes);
+
+        void showAddNote();
+
+        void showNoteDetailUi(String noteId);
+    }
+
+    interface UserActionsListener {
+
+        void loadNotes(boolean forceUpdate);
+
+        void addNewNote();
+
+        void openNoteDetails(@NonNull Note requestedNote);
+    }
 }
