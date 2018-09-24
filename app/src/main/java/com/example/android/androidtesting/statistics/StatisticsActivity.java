4dceed21e60af0d38ec8d1e2fc1db984fd2005f0
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-09-24 10:00:39 +0100

    project structure

---
+++ app/src/main/java/com/example/android/androidtesting/statistics/StatisticsActivity.java
@@ -0,0 +1,4 @@
+package com.example.android.androidtesting.statistics;
+
+public class StatisticsActivity {
+}
---
+++ app/src/main/java/com/example/android/androidtesting/util/ImageFile.java
@@ -0,0 +1,4 @@
+package com.example.android.androidtesting.util;
+
+public interface ImageFile {
+}
