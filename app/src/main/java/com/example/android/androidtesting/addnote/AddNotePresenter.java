10c0cb9dfba162680838fce7e98c2af78b9de22a
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-09-24 11:01:37 +0100

    add note folder

--- app/src/main/java/com/example/android/androidtesting/addnote/AddNotePresenter.java
+++ app/src/main/java/com/example/android/androidtesting/addnote/AddNotePresenter.java
@@ -1,4 +1,77 @@
 package com.example.android.androidtesting.addnote;
 
-public class AddNotePresenter {
+import com.example.android.androidtesting.data.Note;
+import com.example.android.androidtesting.data.NotesRepository;
+import com.example.android.androidtesting.util.ImageFile;
+
+import android.support.annotation.NonNull;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Listens to user actions from the UI ({@link AddNoteFragment}), retrieves the data and updates
+ * the UI as required.
+ */
+
+public class AddNotePresenter implements AddNoteContract.UserActionsListener {
+    @NonNull
+    private final NotesRepository mNotesRepository;
+    @NonNull
+    private final AddNoteContract.View mAddNoteView;
+    @NonNull
+    private final ImageFile mImageFile;
+
+    public AddNotePresenter(@NonNull NotesRepository notesRepository,
+                            @NonNull AddNoteContract.View addNoteView,
+                            @NonNull ImageFile imageFile) {
+        mNotesRepository = checkNotNull(notesRepository);
+        mAddNoteView = checkNotNull(addNoteView);
+        mImageFile = imageFile;
+    }
+
+    @Override
+    public void saveNote(String title, String description) {
+        String imageUrl = null;
+        if (mImageFile.exists()) {
+            imageUrl = mImageFile.getPath();
+        }
+        Note newNote = new Note(title, description, imageUrl);
+        if (newNote.isEmpty()) {
+            mAddNoteView.showEmptyNoteError();
+        } else {
+            mNotesRepository.saveNote(newNote);
+            mAddNoteView.showNotesList();
+        }
+    }
+
+    @Override
+    public void takePicture() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        mImageFile.create(imageFileName, ".jpg");
+        mAddNoteView.openCamera(mImageFile.getPath());
+    }
+
+    @Override
+    public void imageAvailable() {
+        if (mImageFile.exists()) {
+            mAddNoteView.showImagePreview(mImageFile.getPath());
+        } else {
+            imageCaptureFailed();
+        }
+    }
+
+    @Override
+    public void imageCaptureFailed() {
+        captureFailed();
+    }
+
+    private void captureFailed() {
+        mImageFile.delete();
+        mAddNoteView.showImageError();
+    }
 }
