5b1ff45188d7865719e1750b96398425f85c840e
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-09-24 11:22:43 +0100

    EspressoIdlingResource

--- app/src/main/java/com/example/android/androidtesting/util/EspressoIdlingResource.java
+++ app/src/main/java/com/example/android/androidtesting/util/EspressoIdlingResource.java
@@ -1,4 +1,29 @@
 package com.example.android.androidtesting.util;
 
+
+import android.support.test.espresso.IdlingResource;
+
+/**
+ * Contains a static reference to {@link IdlingResource}, only available in the 'mock' build type.
+ */
+
 public class EspressoIdlingResource {
+
+    private static final String RESOURCE = "GLOBAL";
+
+    private static SimpleCountingIdlingResource mCountingIdlingResource =
+            new SimpleCountingIdlingResource(RESOURCE);
+
+    public static void increment() {
+        mCountingIdlingResource.increment();
+    }
+
+    public static void decrement() {
+        mCountingIdlingResource.decrement();
+    }
+
+    public static IdlingResource getIdlingResource() {
+        return mCountingIdlingResource;
+    }
+
 }
