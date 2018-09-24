ef4d6a777f748413877059f02314bfe342f888bd
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-09-24 11:26:12 +0100

    ImageFile

--- app/src/main/java/com/example/android/androidtesting/util/ImageFile.java
+++ app/src/main/java/com/example/android/androidtesting/util/ImageFile.java
@@ -1,4 +1,16 @@
 package com.example.android.androidtesting.util;
 
+import java.io.IOException;
+
+/**
+ * A wrapper for handling image files.
+ */
 public interface ImageFile {
+    void create(String name, String extension) throws IOException;
+
+    boolean exists();
+
+    void delete();
+
+    String getPath();
 }
