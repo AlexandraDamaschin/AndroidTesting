280f32466b69423cb487c448ec707941389e1bf5
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-09-25 11:54:03 +0100

    Test verifies that clicking the ‘Home' icon on the start screen opens the navigation drawer.

--- app/src/androidTest/java/com/example/android/androidtesting/notes/AppNavigationTest.java
+++ app/src/androidTest/java/com/example/android/androidtesting/notes/AppNavigationTest.java
@@ -5,11 +5,21 @@ import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.v4.widget.DrawerLayout;
+import android.view.Gravity;
+
+import com.example.android.androidtesting.R;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.DrawerMatchers.isClosed;
+import static android.support.test.espresso.contrib.DrawerMatchers.isOpen;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.fail;
 
 
@@ -20,10 +30,9 @@ import static org.junit.Assert.fail;
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class AppNavigationTest {
-
+    //todo: Espresso-Intents Test 1: Clicking "Navigate up" opens the Navigation Drawer
     /**
-     * {@link ActivityTestRule} is a JUnit {@link Rule @Rule} to launch your activity under test.
-     *
+     * {@link ActivityTestRule} is a JUnit {@link Rule @Rule} to launch your activity under test
      * <p>
      * Rules are interceptors which are executed for each test method and are important building
      * blocks of Junit tests.
@@ -50,21 +59,25 @@ public class AppNavigationTest {
 //        onView(withId(R.id.no_statistics)).check(matches(withText(expectedNoStatisticsText)));
     }
 
+    /**
+     * Test verifies that clicking the ‘Home' icon on the start screen opens the navigation drawer.
+     */
     @Test
     public void clickOnAndroidHomeIcon_OpensNavigation() {
-        fail("Implement step 9");
-//        // Check that left drawer is closed at startup
-//        onView(withId(R.id.drawer_layout))
-//                .check(matches(isClosed(Gravity.LEFT))); // Left Drawer should be closed.
-//
-//        // Open Drawer
-//        String navigateUpDesc = mActivityTestRule.getActivity()
-//                .getString(android.support.v7.appcompat.R.string.abc_action_bar_up_description);
-//        onView(withContentDescription(navigateUpDesc)).perform(click());
-//
-//        // Check if drawer is open
-//        onView(withId(R.id.drawer_layout))
-//                .check(matches(isOpen(Gravity.LEFT))); // Left drawer is open open.
+        //fail("Implement step 9");
+
+        // Check that left drawer is closed at startup
+        onView(withId(R.id.drawer_layout))
+                .check(matches(isClosed(Gravity.LEFT))); // Left Drawer should be closed.
+
+        // Open Drawer
+        String navigateUpDesc = mActivityTestRule.getActivity()
+                .getString(android.support.v7.appcompat.R.string.abc_action_bar_up_description);
+        onView(withContentDescription(navigateUpDesc)).perform(click());
+
+        // Check if drawer is open
+        onView(withId(R.id.drawer_layout))
+                .check(matches(isOpen(Gravity.LEFT))); // Left drawer is open open.
     }
 
 }
