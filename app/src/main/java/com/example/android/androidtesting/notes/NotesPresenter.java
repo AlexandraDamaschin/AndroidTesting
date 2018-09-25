1f43a1b29104c4224880fdf5cdd541fb9eff83a9
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-09-25 12:29:31 +0100

    Resource is not idle

--- app/src/main/java/com/example/android/androidtesting/notes/NotesPresenter.java
+++ app/src/main/java/com/example/android/androidtesting/notes/NotesPresenter.java
@@ -39,7 +39,7 @@ public class NotesPresenter implements NotesContract.UserActionsListener {
 
         // The network request might be handled in a different thread so make sure Espresso knows
         // that the app is busy until the response is handled.
-        EspressoIdlingResource.increment(); // App is busy until further notice
+        EspressoIdlingResource.increment(); // App is busy until further notice -> resource is not idle
 
         mNotesRepository.getNotes(new NotesRepository.LoadNotesCallback() {
             @Override
